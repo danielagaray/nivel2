3e447fdc06d0df3e443dc962005c99510a4bb87f
Daniela Garay Belo <Daniela Garay Belo>
2017-06-03 15:00:50 -0300

    otra version

---
+++ Proyecto1/proyecto1b.js
@@ -0,0 +1,22 @@
+/*var respuesta=prompt("Queres seguir viendo la clase?");
+if(respuesta=="si" || respuesta=="Si"){
+	alert("Seguimos en clase");
+}else{
+	alert("Adiós!");
+}*/
+
+/*nombre=prompt("Ingresá tu nombre");
+
+switch(nombre){
+	case "camila":
+		alert("Hola profe");
+		break;
+	default:
+		alert("Hola "+nombre);
+		break;
+	}*/
+
+
+for(var i;i<25;i++){
+	console.log("Dar un paso");
+}
