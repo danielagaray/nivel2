383a41194213fd8be07ade8f2ecc7a95d9a1cbcd
Daniela Garay Belo <Daniela Garay Belo>
2017-06-03 15:25:33 -0300

    another commit

---
+++ Proyecto1/ejercicio.js
@@ -0,0 +1,9 @@
+
+var dias=["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"];
+
+for(i in dias){
+	if(dias[i]=="Sábado" || dias[i]=="Domingo"){
+		console.log("Atención! es: "+ dias[i]);
+	}
+}
+
